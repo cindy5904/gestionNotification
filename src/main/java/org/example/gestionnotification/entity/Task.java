79d4809d400f20619af486188771e78634ec24d5
Cindy Telmon <telcindy59@gmail.com>
2024-08-12 16:49:42 +0200

    ajout Filtre

--- src/main/java/org/example/gestionnotification/entity/Task.java
+++ src/main/java/org/example/gestionnotification/entity/Task.java
@@ -10,12 +10,9 @@ public class Task {
     private String name;
     private boolean status;
 
-
     public Task( String name, boolean status) {
-
         this.name = name;
         this.status = status;
-
     }
 
     public Task() {
